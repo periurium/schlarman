66fbe3cee22e55757a8f212d63af85bed9e53a57
Periurium <44757288+periurium@users.noreply.github.com>
2020-06-18 00:05:20 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php ?>
+
+<!DOCTYPE html>
+<!--[if lte IE 6]><html class="preIE7 preIE8 preIE9"><![endif]-->
+<!--[if IE 7]><html class="preIE8 preIE9"><![endif]-->
+<!--[if IE 8]><html class="preIE9"><![endif]-->
+<!--[if gte IE 9]><!--><html><!--<![endif]-->
+  <head>
+    <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Tristan Schlarman - Freelance Web Developer</title>
+  <meta name="author" content="Tristan Schlarman">
+  <meta name="description" content="My personal portfolio of my work and my offering for web development services.">
+  <meta name="keywords" content="tristan, schlarman, web, development, services, html, css, javascript, php, mysql, modern, ui, ux">
+  <link rel="stylesheet" href="assets/css/master.css" type="text/css">
+  </head>
+  <body>
+    <div id="landing">
+    
+    </div> 
+  </body>
+</html>
